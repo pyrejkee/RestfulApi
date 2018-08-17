5bfb16916f82d7cdff2e575dd995235054118438
Kiryl_Kiryanchykau <kiryl_kiryanchykau@epam.com>
2018-08-17 13:48:43 +0300

    Change returning types from JsonResult to StatusCodes

--- src/Library.API/Controllers/AuthorsController.cs
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -23,16 +23,22 @@ namespace Library.API.Controllers
             var authorsFromRepo = _libraryRepository.GetAuthors();
             var authors = Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
 
-            return new JsonResult(authors);
+            return Ok(authors);
         }
 
         [HttpGet("{id}")]
         public IActionResult GetAuthor(Guid id)
         {
             var authorFromRepo = _libraryRepository.GetAuthor(id);
+
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
             var author = Mapper.Map<AuthorDto>(authorFromRepo);
 
-            return new JsonResult(author);
+            return Ok(author);
         }
     }
 }
