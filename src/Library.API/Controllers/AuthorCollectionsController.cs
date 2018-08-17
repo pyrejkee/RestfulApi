67a1701c8dd2db4e307606e0d81c851da4e73b63
Kiryl_Kiryanchykau <kiryl_kiryanchykau@epam.com>
2018-08-17 16:34:36 +0300

    Add AuthorCollections controller for creating many authors in one go

---
+++ src/Library.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Library.API.Entities;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController : Controller
+    {
+        private readonly ILibraryRepository _libraryRepository;
+
+        public AuthorCollectionsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthorCollection([FromBody] IEnumerable<AuthorForCreationDto> authorCollection)
+        {
+            if (authorCollection == null)
+            {
+                return BadRequest();
+            }
+
+            var authorEntities = Mapper.Map<IEnumerable<Author>>(authorCollection);
+
+            foreach (var author in authorEntities)
+            {
+                _libraryRepository.AddAuthor(author);
+            }
+
+            if (!_libraryRepository.Save())
+            {
+                throw new Exception("Creating author collection failed on save.");
+            }
+
+            return Ok();
+        }
+    }
+}
